3e04435d0fba60a458c166070bb5431caa77bfe1
Caleb McBride <atfbassman@yahoo.com>
2024-01-28 23:35:56 -0700

    Props and Component State

--- udemy1/src/App.js
+++ udemy1/src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
+import NavBar from './Components/layout/NavBar'
+import UserItem from './Components/users/UserItem';
 import './App.css';
 
 class App extends Component {
   render() {
-   
-
-  return (
-    <div className="App">
-     <h1>Hello</h1>
+   return (
+    <div className='App'>
+    <NavBar />
+    <UserItem />
     </div>
-   
     );  
+    
   }
 }
 
