8062fc71e539e294f6670f9a388791e3c7d542cf
Caleb McBride <atfbassman@yahoo.com>
2024-01-28 22:35:45 -0700

    nav addition

--- udemy1/src/App.js
+++ udemy1/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 
 class App extends Component {
   render() {
+   
+
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+     <h1>Hello</h1>
     </div>
-    <div>I want to stop being broke.</div>
+   
     );  
   }
 }
