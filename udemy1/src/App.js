540a3e1d8498d9387c97fb36289ab8518a1a2c2e
Caleb McBride <atfbassman@yahoo.com>
2024-02-02 01:09:47 -0700

    HTTP Requests & Updating State

--- udemy1/src/App.js
+++ udemy1/src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import NavBar from './Components/layout/NavBar'
 import Users from './Components/users/Users';
 import './App.css';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get('https://api.github.com/users');
+
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
    return (
     <div className='App'>
     <NavBar />
       <div className="container">
-        <Users />
+        <Users loading={this.state.loading} users={this.state.users} />
       </div>
     </div>
     );  
